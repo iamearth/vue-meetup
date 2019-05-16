2dfd492b76ec4ddbb1c3a6bb60ecbaf6dc2eb423
Earth Suwatluckul <hi@iamearth.co>
2019-05-16 00:24:12 -0700

    project init

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,43 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
-</template>
+  <template>
+    <v-app>
+    <!-- side navigation ----------------------------------------->
+      <v-navigation-drawer temporary fixed v-model="sideNav">
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon>supervisor_icon</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>View Meetups</v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+    <!-- top  navigation ----------------------------------------->
+      <v-toolbar>
+        <v-toolbar-title>DevMeetup</v-toolbar-title>
+        <!-- adds hamburger icon -->
+        <v-toolbar-side-icon 
+          @click="sideNav = !sideNav"
+          class="hidden-sm-and-up">
+        </v-toolbar-side-icon>
+        <v-spacer><!-- space between logo and navigation --></v-spacer>
+        <v-toolbar-items class="hidden-xs-only">
+          <v-btn flat>
+            <v-icon left>supervisor_account</v-icon>
+            View Meetups
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+      <main></main>
+    </v-app>
+  </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      sideNav: false
     }
   }
 }
-</style>
+</script>
